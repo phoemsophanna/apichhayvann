8c3a90d14813582e6c7b38839728400281476c5c
phoemsophanna <phannaphoem@gmail.com>
2026-02-03 09:48:32 +0700

    Update some code

--- app/Http/Controllers/AdminController/IndividualController.php
+++ app/Http/Controllers/AdminController/IndividualController.php
@@ -72,6 +72,7 @@ class IndividualController extends Controller
     {
         $model = Individual::findOrFail($request->id);
         $model->create_at = Carbon::parse($model->created_at)->format("d F Y");
+        $model->date = Carbon::parse($model->date)->format("d F Y");
         $model->privacy = json_decode($model->privacy);
         return response()->json([
             'message' => 'Get detail success.',
