d73949b952e30a39519967bd542229c0c0031e60
phoemsophanna <phannaphoem@gmail.com>
2026-02-16 10:54:13 +0700

    Update some code

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -48,18 +48,6 @@ return [
             ],
         ],
 
-        'reverb' => [
-            'driver' => 'reverb',
-            'key' => env('REVERB_APP_KEY'),
-            'secret' => env('REVERB_APP_SECRET'),
-            'app_id' => env('REVERB_APP_ID'),
-            'options' => [
-                'host' => env('REVERB_HOST', '0.0.0.0'),
-                'port' => env('REVERB_PORT', 8080),
-                'scheme' => env('REVERB_SCHEME', 'https'),
-            ],
-        ],
-
         'ably' => [
             'driver' => 'ably',
             'key' => env('ABLY_KEY'),
