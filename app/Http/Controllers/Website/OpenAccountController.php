873b82113c1ee53421aab213a18d658ab6b0d197
phoemsophanna <phannaphoem@gmail.com>
2026-02-19 16:41:08 +0700

    Update some code

--- app/Http/Controllers/Website/OpenAccountController.php
+++ app/Http/Controllers/Website/OpenAccountController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Services\FileService;
 use App\Models\Corporate;
+use App\Models\Individual;
 use App\Models\SiteSetting;
 
 class OpenAccountController extends Controller
