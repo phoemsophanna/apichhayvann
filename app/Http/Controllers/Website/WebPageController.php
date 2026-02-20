ab26b4a38f927bb490d4552aa358848023a4e8e8
phoemsophanna <phannaphoem@gmail.com>
2026-02-20 14:48:09 +0700

    Update some code

--- app/Http/Controllers/Website/WebPageController.php
+++ app/Http/Controllers/Website/WebPageController.php
@@ -402,7 +402,8 @@ class WebPageController extends Controller
                 MAX(bid) as high, 
                 SUBSTRING_INDEX(GROUP_CONCAT(bid ORDER BY recorded_at ASC), ',', 1) as open,
                 SUBSTRING_INDEX(GROUP_CONCAT(bid ORDER BY recorded_at DESC), ',', 1) as close,
-                recorded_at as time_bucket
+                FLOOR(UNIX_TIMESTAMP(recorded_at) / 60) * 60 as time_bucket,
+                recorded_at
             ")
             ->where('recorded_at', '>', now()->subHours(2))
             ->groupBy('pair', 'time_bucket')
