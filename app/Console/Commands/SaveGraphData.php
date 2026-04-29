3a438eb150f16921c91edd3e40534fef6569f5be
phoemsophanna <phannaphoem@gmail.com>
2026-04-29 11:12:17 +0700

    Update some code

--- app/Console/Commands/SaveGraphData.php
+++ app/Console/Commands/SaveGraphData.php
@@ -5,6 +5,7 @@ namespace App\Console\Commands;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\Redis;
 use Illuminate\Support\Facades\Cache;
+use Carbon\Carbon;
 
 class SaveGraphData extends Command
 {
@@ -28,57 +29,58 @@ class SaveGraphData extends Command
     public function handle()
     {
         while (true) {
-
+            $day = Carbon::now()->format("D");
             $startTime = microtime(true);
-
-            $data = Cache::get('external_latest');
-
-            if ($data) {
-                try {
-                    foreach ($data as $item) {
-                        if ($item['PAIR'] === 'XAUUSD') {
-
-                            $timestamp = now()->timestamp;
-
-                            $record = json_encode([
-                                'pair' => $item['PAIR'],
-                                'bid'  => $item['BID'],
-                                'ask'  => $item['ASK'],
-                                'recorded_at' => now()->toDateTimeString()
-                            ]);
-
-                            Redis::zadd('price_history_xauusd', $timestamp, $record);
-
-                            Redis::zremrangebyscore(
-                                'price_history_xauusd',
-                                0,
-                                now()->subHours(2)->timestamp
-                            );
+            if ($day != "Sat" && $day != "Sun") {
+                $data = Cache::get('external_latest');
+    
+                if ($data) {
+                    try {
+                        foreach ($data as $item) {
+                            if ($item['PAIR'] === 'XAUUSD') {
+    
+                                $timestamp = now()->timestamp;
+    
+                                $record = json_encode([
+                                    'pair' => $item['PAIR'],
+                                    'bid'  => $item['BID'],
+                                    'ask'  => $item['ASK'],
+                                    'recorded_at' => now()->toDateTimeString()
+                                ]);
+    
+                                Redis::zadd('price_history_xauusd', $timestamp, $record);
+    
+                                Redis::zremrangebyscore(
+                                    'price_history_xauusd',
+                                    0,
+                                    now()->subHours(2)->timestamp
+                                );
+                            }
+    
+                            if ($item['PAIR'] === 'XAGUSD') {
+    
+                                $timestamp = now()->timestamp;
+    
+                                $record = json_encode([
+                                    'pair' => $item['PAIR'],
+                                    'bid'  => $item['BID'],
+                                    'ask'  => $item['ASK'],
+                                    'recorded_at' => now()->toDateTimeString()
+                                ]);
+    
+                                Redis::zadd('price_history_xagusd', $timestamp, $record);
+    
+                                Redis::zremrangebyscore(
+                                    'price_history_xagusd',
+                                    0,
+                                    now()->subHours(2)->timestamp
+                                );
+                            }
                         }
-
-                        if ($item['PAIR'] === 'XAGUSD') {
-
-                            $timestamp = now()->timestamp;
-
-                            $record = json_encode([
-                                'pair' => $item['PAIR'],
-                                'bid'  => $item['BID'],
-                                'ask'  => $item['ASK'],
-                                'recorded_at' => now()->toDateTimeString()
-                            ]);
-
-                            Redis::zadd('price_history_xagusd', $timestamp, $record);
-
-                            Redis::zremrangebyscore(
-                                'price_history_xagusd',
-                                0,
-                                now()->subHours(2)->timestamp
-                            );
-                        }
+                    } catch (Exception $th) {
+                        Log::info('Error: ' . $error->getMessage());
+                        return false;
                     }
-                } catch (Exception $th) {
-                    Log::info('Error: ' . $error->getMessage());
-                    return false;
                 }
             }
 
