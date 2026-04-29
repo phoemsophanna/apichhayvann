8a5854d31e7fbfd8d9abfa1caec30e8198411530
phoemsophanna <phannaphoem@gmail.com>
2026-04-29 11:04:11 +0700

    Update some code

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -12,7 +12,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule): void
     {
-        $schedule->command('app:save-graph-data')->everyMinute();
+        // $schedule->command('app:save-graph-data')->everyMinute();
     }
 
     /**
