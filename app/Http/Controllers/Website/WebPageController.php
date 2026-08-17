284f1d0904ecf90cbb56bbfaab59b07f338b698a
phoemsophanna <phannaphoem@gmail.com>
2026-08-17 09:55:51 +0700

    Update

--- app/Http/Controllers/Website/WebPageController.php
+++ app/Http/Controllers/Website/WebPageController.php
@@ -398,12 +398,11 @@ class WebPageController extends Controller
 
     public function seoSiteSetting(Request $request) {
         $lang = $request->header("Accept-Language");
-        // $sites = SiteSetting::where("type", $request->page)->first();
-        // $sites = json_decode($sites->content);
+        $sites = SiteSetting::where("type", $request->page)->first();
+        $sites = json_decode($sites->content);
         return response()->json([
             "status" => "success",
-            // "sites" => $sites
-            "page" => $request->page
+            "sites" => $sites
         ], 200);
     }
 
